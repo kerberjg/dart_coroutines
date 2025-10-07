0ee148b58f4e9e1e14c7917bd354f78c49f512e7
Jamie Kerber <kerber.jg@gmail.com>
2025-10-08 01:13:21 +0200

    Implemented async coroutines

---
+++ lib/async.dart
@@ -0,0 +1,83 @@
+library coroutines;
+
+import 'dart:async';
+
+typedef CoroutineAsyncValue<T> = Stream<T>;
+typedef CoroutineAsync<T> = CoroutineAsyncValue<T> Function();
+typedef CoroutineAsyncInstance<T> = StreamIterator<T>;
+
+mixin class CoroutineExecutor {
+  /// Map of running coroutines
+  /// Key: coroutine hash
+  /// Value: coroutine instance
+  final Map<int, CoroutineAsyncInstance> _runningCoroutines = {};
+
+  /// The count of running coroutines
+  int get countCoroutines => _runningCoroutines.length;
+
+  /// Adds a coroutine to the executor without starting it
+  /// Does nothing if the coroutine is already running
+  void addCoroutine<T>(CoroutineAsync<T> coroutine) {
+    _getOrAddCoroutine(coroutine);
+  }
+
+  @pragma('vm:always-consider-inlining')
+  CoroutineAsyncInstance<T> _getOrAddCoroutine<T>(CoroutineAsync<T> coroutine) {
+    final int id = coroutine.hashCode;
+    if (_runningCoroutines[id] == null) {
+      final stream = coroutine();
+      print(stream.runtimeType);
+      _runningCoroutines[id] = StreamIterator<T>(stream);
+    }
+
+    CoroutineAsyncInstance instance = _runningCoroutines[id]!;
+    print(instance.runtimeType);
+    return _runningCoroutines[id]! as CoroutineAsyncInstance<T>;
+  }
+
+  /// Starts or continues a coroutine
+  /// It starts or continues its execution depending on its current state
+  /// If the coroutine is paused, it resumes execution from the last yield point
+  Future<T?> runCoroutine<T>(CoroutineAsync<T> coroutine) {
+    return _stepCoroutine(coroutine.hashCode, _getOrAddCoroutine(coroutine));
+  }
+
+  /// Continues all coroutines in this executor
+  Future<void> runAllCoroutines() async {
+    for (final id in _runningCoroutines.keys) {
+      await _stepCoroutine(id, _runningCoroutines[id]!);
+    }
+  }
+
+  /// Returns whether a coroutine is currently running
+  ///
+  bool isCoroutineRunning(CoroutineAsync coroutine) {
+    final int id = coroutine.hashCode;
+    return _runningCoroutines.containsKey(id);
+  }
+
+  @pragma('vm:always-consider-inlining')
+  Future<T?> _stepCoroutine<T>(int id, CoroutineAsyncInstance<T> instance) async {
+    final bool hasNext = await instance.moveNext();
+
+    if (hasNext) {
+      return instance.current;
+    } else {
+      // coroutine has finished execution
+      await instance.cancel();
+      _runningCoroutines.remove(id);
+      return null;
+    }
+  }
+
+  /// Stops a coroutine
+  void stopCoroutine(CoroutineAsync coroutine) {
+    final int id = coroutine.hashCode;
+    _runningCoroutines.remove(id);
+  }
+
+  /// Stops all coroutines in this executor
+  void stopAllCoroutines() {
+    _runningCoroutines.clear();
+  }
+}
---
+++ test/async.dart
@@ -0,0 +1,160 @@
+import 'package:test/test.dart';
+import 'package:coroutines/async.dart';
+
+void main() {
+  group('CoroutineAsync', () {
+    test('can be run at least once', () async {
+      final executor = CoroutineExecutor();
+
+      CoroutineAsyncValue<int> myCoroutine() async* {
+        int counter = 1;
+        yield counter;
+      }
+
+      final int result = await executor.runCoroutine(myCoroutine) ?? -1;
+      expect(result, equals(1));
+    });
+  });
+
+  test('can continue execution', () async {
+    final executor = CoroutineExecutor();
+
+    CoroutineAsyncValue<int> myCoroutine() async* {
+      int counter = 1;
+      yield counter;
+      counter = 2;
+      yield counter;
+      counter = 3;
+      yield counter;
+    }
+
+    final int result1 = await executor.runCoroutine(myCoroutine) ?? -1;
+    expect(result1, equals(1));
+
+    final int result2 = await executor.runCoroutine(myCoroutine) ?? -1;
+    expect(result2, equals(2));
+
+    final int result3 = await executor.runCoroutine(myCoroutine) ?? -1;
+    expect(result3, equals(3));
+  });
+
+  test('can be stopped and restarted', () async {
+    final executor = CoroutineExecutor();
+
+    CoroutineAsyncValue<int> myCoroutine() async* {
+      int counter = 1;
+      yield counter;
+      counter = 2;
+      yield counter;
+      counter = 3;
+      yield counter;
+    }
+
+    final int result1 = await executor.runCoroutine(myCoroutine) ?? -1;
+    expect(result1, equals(1));
+
+    executor.stopCoroutine(myCoroutine);
+
+    final int result2 = await executor.runCoroutine(myCoroutine) ?? -1;
+    expect(result2, equals(1));
+  });
+
+  test("addCoroutine doesn't start it", () async {
+    final executor = CoroutineExecutor();
+
+    int counter = 0;
+    CoroutineAsyncValue<int> myCoroutine() async* {
+      counter = 1;
+      yield counter;
+      counter = 2;
+      yield counter;
+      counter = 3;
+      yield counter;
+    }
+
+    expect(counter, equals(0));
+
+    executor.addCoroutine(myCoroutine);
+    expect(counter, equals(0));
+
+    await executor.runCoroutine(myCoroutine);
+    expect(counter, equals(1));
+  });
+
+  test('runAllCoroutines continues all coroutines', () async {
+    final executor = CoroutineExecutor();
+
+    int counterA = 0;
+    CoroutineAsyncValue<int> myCoroutine1() async* {
+      counterA = 1;
+      yield counterA;
+      counterA = 2;
+      yield counterA;
+      counterA = 3;
+      yield counterA;
+    }
+
+    int counterB = 0;
+    CoroutineAsyncValue<int> myCoroutine2() async* {
+      counterB = 3;
+      yield counterB;
+      counterB = 4;
+      yield counterB;
+      counterB = 5;
+      yield counterB;
+    }
+
+    executor.addCoroutine(myCoroutine1);
+    executor.addCoroutine(myCoroutine2);
+
+    await executor.runAllCoroutines();
+    expect(counterA, equals(1));
+    expect(counterB, equals(3));
+
+    await executor.runAllCoroutines();
+    expect(counterA, equals(2));
+    expect(counterB, equals(4));
+  });
+
+  test('finished coroutine is removed from running coroutines', () async {
+    final executor = CoroutineExecutor();
+
+    CoroutineAsyncValue<int> myCoroutine() async* {
+      int counter = 1;
+      yield counter;
+      counter = 2;
+      yield counter;
+    }
+
+    expect(executor.countCoroutines, equals(0));
+
+    await executor.runCoroutine(myCoroutine); // value 1
+    expect(executor.countCoroutines, equals(1));
+
+    await executor.runCoroutine(myCoroutine); // value 2
+    await executor.runCoroutine(myCoroutine); // value null, coroutine finished
+    expect(executor.countCoroutines, equals(0));
+  });
+
+  test('correctly reports whether a coroutine is running', () async {
+    final executor = CoroutineExecutor();
+
+    CoroutineAsyncValue<int> myCoroutine() async* {
+      int counter = 1;
+      yield counter;
+      counter = 2;
+      yield counter;
+    }
+
+    expect(executor.isCoroutineRunning(myCoroutine), isFalse);
+
+    await executor.runCoroutine(myCoroutine); // value 1
+    expect(executor.isCoroutineRunning(myCoroutine), isTrue);
+
+    await executor.runCoroutine(myCoroutine); // value 2
+    expect(executor.isCoroutineRunning(myCoroutine), isTrue);
+
+    await executor.runCoroutine(myCoroutine); // value null, coroutine finished
+    expect(executor.isCoroutineRunning(myCoroutine), isFalse);
+  });
+}
